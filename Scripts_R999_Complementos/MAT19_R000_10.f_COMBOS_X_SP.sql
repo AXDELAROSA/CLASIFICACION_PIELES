54b620e0f68830c37b96d6ec16ae7edfc73063af
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2019-12-27 18:25:15 -0700

    NUEVAS MODIFICACIONES

--- Scripts_R999_Complementos/MAT19_R000_10.f_COMBOS_X_SP.sql
+++ Scripts_R999_Complementos/MAT19_R000_10.f_COMBOS_X_SP.sql
@@ -13,7 +13,7 @@ GO
 -- //////////////////////////////////////////////////////////////
 
 
-
+/* CARGA COMBO DE COLORES */
 -- EXECUTE [PG_CB_COLOUR] 001,144, 0
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CB_COLOUR]') AND type in (N'P', N'PC'))
 	DROP PROCEDURE [dbo].[PG_CB_COLOUR]
@@ -55,7 +55,7 @@ GO
 
 
 
-
+/* CARGA COMBO DE LOCACIONES */
 -- EXECUTE [PG_CB_IMLOCFIL_SQL] 001,144, 0
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CB_IMLOCFIL_SQL]') AND type in (N'P', N'PC'))
 	DROP PROCEDURE [dbo].[PG_CB_IMLOCFIL_SQL]
@@ -94,6 +94,48 @@ AS
 	-- ////////////////////////////////////////////////////
 GO
 
+
+/* CARGA COMBO DE ORDENES */
+
+-- EXECUTE [PG_CB_JOBNO_SQL] 001,144, 0, 'Table 01'      
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CB_JOBNO_SQL]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[PG_CB_JOBNO_SQL]
+GO
+
+
+CREATE PROCEDURE [dbo].[PG_CB_JOBNO_SQL]
+	@PP_K_SISTEMA_EXE			INT,
+	@PP_K_USUARIO				INT,
+	--============================
+	@PP_L_CON_TODOS				INT,
+	@PP_MACHINE					VARCHAR(15)
+AS
+	DECLARE @VP_TA_CATALOGO	AS TABLE
+				(	TA_K_CATALOGO		INT IDENTITY(1,1) NOT NULL,
+					TA_D_CATALOGO		VARCHAR(50))
+	
+	INSERT INTO @VP_TA_CATALOGO 
+
+	SELECT	LTRIM(RTRIM(jobno))		AS D_COMBOBOX	
+	FROM	ccjobhdr_sql 
+	WHERE	status='P'
+	AND		machine=@PP_MACHINE
+	
+	--IF @PP_L_CON_TODOS=1
+	--	INSERT INTO @VP_TA_CATALOGO
+	--			( TA_K_CATALOGO,	TA_D_CATALOGO	)
+	--		VALUES
+	--			( -1,				'( TODOS )'	)
+
+	SELECT	TA_K_CATALOGO	AS K_COMBOBOX,
+				TA_D_CATALOGO	AS D_COMBOBOX 
+	FROM	@VP_TA_CATALOGO
+	ORDER BY  TA_D_CATALOGO 
+
+	-- ==========================================
+		
+	-- ////////////////////////////////////////////////////
+GO
 
 
 
