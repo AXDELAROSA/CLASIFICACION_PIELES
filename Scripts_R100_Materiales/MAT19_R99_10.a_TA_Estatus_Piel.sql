40257fef8b1ecede8b5f36a170cd43facdeb4ebc
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-01-13 17:30:02 -0700

    cambios para clasificacion de pieles

---
+++ Scripts_R100_Materiales/MAT19_R99_10.a_TA_Estatus_Piel.sql
@@ -0,0 +1,147 @@
+-- //////////////////////////////////////////////////////////////
+-- // ARCHIVO:			
+-- //////////////////////////////////////////////////////////////
+-- // BASE DE DATOS:	DATA_02Pruebas
+-- // MODULO:			CLASIFICACION PIEL
+-- // OPERACION:		LIBERACION / TABLAS
+-- //////////////////////////////////////////////////////////////
+-- // Autor:			FEG
+-- // Fecha creación:	10/ENE/2020
+-- ////////////////////////////////////////////////////////////// 
+
+USE DATA_02Pruebas 
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- // DROPs
+-- //////////////////////////////////////////////////////////////
+
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PIEL_CLASIFICACION]') AND type in (N'U'))
+	DROP TABLE [dbo].[PIEL_CLASIFICACION]
+GO
+
+
+-- //////////////////////////////////////////////////////////////
+-- // PIEL_CLASIFICACION
+-- //////////////////////////////////////////////////////////////
+
+
+CREATE TABLE [dbo].[PIEL_CLASIFICACION] (
+	[K_PIEL_CLASIFICACION]	[INT]				NOT NULL,
+	[D_PIEL_CLASIFICACION]	[VARCHAR] (100)		NOT NULL,
+	[S_PIEL_CLASIFICACION]	[VARCHAR] (10)		NOT NULL,
+	[O_PIEL_CLASIFICACION]	[INT]				NOT NULL,
+	[C_PIEL_CLASIFICACION]	[VARCHAR] (255)		NOT NULL,
+	[L_PIEL_CLASIFICACION]	[INT]				NOT NULL
+) ON [PRIMARY]
+GO
+
+
+-- //////////////////////////////////////////////////////////////
+
+
+ALTER TABLE [dbo].[PIEL_CLASIFICACION]
+	ADD CONSTRAINT [PK_PIEL_CLASIFICACION]
+		PRIMARY KEY CLUSTERED ([K_PIEL_CLASIFICACION])
+GO
+
+
+CREATE UNIQUE NONCLUSTERED 
+	INDEX [UN_PIEL_CLASIFICACION_01_DESCRIPCION] 
+	   ON [dbo].[PIEL_CLASIFICACION] ( [D_PIEL_CLASIFICACION] )
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+
+
+-- //////////////////////////////////////////////////////////////
+
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CI_PIEL_CLASIFICACION]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[PG_CI_PIEL_CLASIFICACION]
+GO
+
+
+CREATE PROCEDURE [dbo].[PG_CI_PIEL_CLASIFICACION]
+	@PP_L_DEBUG						INT,
+	@PP_K_SISTEMA_EXE				INT,
+	-- ========================================
+	@PP_K_PIEL_CLASIFICACION		INT,
+	@PP_D_PIEL_CLASIFICACION		VARCHAR(100),
+	@PP_S_PIEL_CLASIFICACION		VARCHAR(10),
+	@PP_O_PIEL_CLASIFICACION		INT,
+	@PP_C_PIEL_CLASIFICACION		VARCHAR(255),
+	@PP_L_PIEL_CLASIFICACION		INT
+AS
+	
+	-- ===============================
+
+	DECLARE @VP_K_EXISTE	INT
+
+	SELECT	@VP_K_EXISTE =	K_PIEL_CLASIFICACION
+							FROM	PIEL_CLASIFICACION
+							WHERE	K_PIEL_CLASIFICACION=@PP_K_PIEL_CLASIFICACION
+
+	-- ===============================
+
+	IF @VP_K_EXISTE IS NULL
+		INSERT INTO PIEL_CLASIFICACION	
+			(	K_PIEL_CLASIFICACION,				D_PIEL_CLASIFICACION, 
+				S_PIEL_CLASIFICACION,				O_PIEL_CLASIFICACION,
+				C_PIEL_CLASIFICACION,
+				L_PIEL_CLASIFICACION				)		
+		VALUES	
+			(	@PP_K_PIEL_CLASIFICACION,			@PP_D_PIEL_CLASIFICACION,	
+				@PP_S_PIEL_CLASIFICACION,			@PP_O_PIEL_CLASIFICACION,
+				@PP_C_PIEL_CLASIFICACION,
+				@PP_L_PIEL_CLASIFICACION			)
+	ELSE
+		UPDATE	PIEL_CLASIFICACION
+		SET		D_PIEL_CLASIFICACION	= @PP_D_PIEL_CLASIFICACION,	
+				S_PIEL_CLASIFICACION	= @PP_S_PIEL_CLASIFICACION,			
+				O_PIEL_CLASIFICACION	= @PP_O_PIEL_CLASIFICACION,
+				C_PIEL_CLASIFICACION	= @PP_C_PIEL_CLASIFICACION,
+				L_PIEL_CLASIFICACION	= @PP_L_PIEL_CLASIFICACION	
+		WHERE	K_PIEL_CLASIFICACION=@PP_K_PIEL_CLASIFICACION
+
+	-- =========================================================
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+
+
+
+-- ===============================================
+SET NOCOUNT ON
+-- ===============================================
+
+EXECUTE [dbo].[PG_CI_PIEL_CLASIFICACION] 0, 0, 0, '(SIN CLASIFICAR)',		'?????', 1, '', 1
+EXECUTE [dbo].[PG_CI_PIEL_CLASIFICACION] 0, 0, 1, 'BUENA',					'BUENA', 1, '', 1
+EXECUTE [dbo].[PG_CI_PIEL_CLASIFICACION] 0, 0, 2, 'REGULAR',				'REGUL', 1, '', 1
+EXECUTE [dbo].[PG_CI_PIEL_CLASIFICACION] 0, 0, 3, 'MALA',					'MALA', 1, '', 1
+GO
+-- ===============================================
+SET NOCOUNT OFF
+-- ===============================================
+
+GO
+
+
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
