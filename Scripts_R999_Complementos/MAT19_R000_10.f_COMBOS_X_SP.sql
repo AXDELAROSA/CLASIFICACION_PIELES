7ed48efad8e98ca93e2d13c2909d547471d2c8a9
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2020-02-14 16:26:44 -0700

    DATABASE UNIFICATION

--- Scripts_R999_Complementos/MAT19_R000_10.f_COMBOS_X_SP.sql
+++ Scripts_R999_Complementos/MAT19_R000_10.f_COMBOS_X_SP.sql
@@ -54,57 +54,9 @@ AS
 GO
 
 
-
-/* CARGA COMBO DE LOCACIONES */
--- EXECUTE [PG_CB_IMLOCFIL_SQL] 001,144, 0
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CB_IMLOCFIL_SQL]') AND type in (N'P', N'PC'))
-	DROP PROCEDURE [dbo].[PG_CB_IMLOCFIL_SQL]
-GO
-
-
-CREATE PROCEDURE [dbo].[PG_CB_IMLOCFIL_SQL]
-	@PP_K_SISTEMA_EXE			INT,
-	@PP_K_USUARIO				INT,
-	--============================
-	@PP_L_CON_TODOS				INT
-AS
-
-	DECLARE @VP_TA_CATALOGO	AS TABLE
-						(	TA_K_CATALOGO		INT IDENTITY(1,1) NOT NULL,
-							TA_D_CATALOGO		VARCHAR(50))
-
-	IF @PP_L_CON_TODOS=1
-		BEGIN	
-			INSERT INTO @VP_TA_CATALOGO 
-			SELECT	LTRIM(RTRIM(alt1_loc))		AS D_COMBOBOX	
-			FROM	imlocfil_sql 
-		END
-	ELSE
-		BEGIN
-			INSERT INTO @VP_TA_CATALOGO 
-			SELECT	LTRIM(RTRIM(alt1_loc))		AS D_COMBOBOX	
-			FROM	imlocfil_sql 
-			WHERE	SUBSTRING(LTRIM(RTRIM(alt1_loc)),1,1) = 'T'
-			UNION
-			SELECT	LTRIM(RTRIM(alt1_loc))		AS D_COMBOBOX	
-			FROM	imlocfil_sql 
-			WHERE	 LTRIM(RTRIM(alt1_loc)) = 'MHI'
-		END
-	
-	SELECT	TA_K_CATALOGO	AS K_COMBOBOX,
-			TA_D_CATALOGO	AS D_COMBOBOX 
-	FROM	@VP_TA_CATALOGO
-	ORDER BY  TA_D_CATALOGO 
-
-	-- ==========================================
-		
-	-- ////////////////////////////////////////////////////
-GO
-
-
 /* CARGA COMBO DE ORDENES */
 
--- EXECUTE [PG_CB_JOBNO_SQL] 001,144, 0, 'Table 01'      
+-- EXECUTE [PG_CB_JOBNO_SQL] 001,144, 0, ''      
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_CB_JOBNO_SQL]') AND type in (N'P', N'PC'))
 	DROP PROCEDURE [dbo].[PG_CB_JOBNO_SQL]
 GO
