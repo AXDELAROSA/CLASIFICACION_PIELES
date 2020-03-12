d49ca6d2b8b96cd0b5584f32f7f1726bde86219b
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-03-11 18:22:46 -0600

    cambios 11032020

--- Scripts_R010_Piel_Seguimiento/MAT_R100_10.a_TA_PIEL_LOG.sql
+++ Scripts_R010_Piel_Seguimiento/MAT_R100_10.a_TA_PIEL_LOG.sql
@@ -140,6 +140,7 @@ EXECUTE [dbo].[PG_CI_TIPO_PIEL_LOG] 0, 0, 4, 'TRANSFERENCIA A ORDEN',			'TRANSF_
 EXECUTE [dbo].[PG_CI_TIPO_PIEL_LOG] 0, 0, 5, 'TRANSFERENCIA A FOLIO',			'TRANSF_FOL', 5, '', 1
 EXECUTE [dbo].[PG_CI_TIPO_PIEL_LOG] 0, 0, 6, 'DEVOLUCION',						'DEVOLUCION', 6, '', 1
 EXECUTE [dbo].[PG_CI_TIPO_PIEL_LOG] 0, 0, 7, 'ISSUE_OUT',						'ISSUE_OUT', 7, '', 1
+EXECUTE [dbo].[PG_CI_TIPO_PIEL_LOG] 0, 0, 8, 'REIMPRESION',						'REIMPRESO', 8, '', 1
 GO
 -- ===============================================
 SET NOCOUNT OFF
