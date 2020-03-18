e4419b7c74a43919e12147bda91ef337e341ce0e
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-03-18 17:39:53 -0600

    Cambios 18032020

--- Scripts_R100_Materiales/TEST_VALIDAR_PIEL_CORTADA.sql
+++ Scripts_R100_Materiales/TEST_VALIDAR_PIEL_CORTADA.sql
@@ -8,6 +8,7 @@ FROM PIEL_LOG
 INNER JOIN users_pearl ON PIEL_LOG.K_USUARIO_ALTA = users_pearl.codigo
 INNER JOIN TIPO_PIEL_LOG ON PIEL_LOG.K_TIPO_PIEL_LOG=TIPO_PIEL_LOG.K_TIPO_PIEL_LOG
 /*
+USE DATA_02Pruebas
 TRUNCATE TABLE [PIEL_A_TRANSFERIR]
 
 DELETE	PIEL_A_TRANSFERIR_TRANSACCION
@@ -18,15 +19,15 @@ WHERE	K_PIEL_A_TRANSFERIR_TRANSACCION = 2
 
 select * from HIDESHDR_SQL   
  inner join HIDESLIN_SQL on  HIDESLIN_SQL.FILENO = HIDESHDR_SQL.FILENO
-AND	LTRIM(RTRIM(PLOT)) = '32782' 
- AND LTRIM(RTRIM(HIDE)) IN   ('0631')
+AND	LTRIM(RTRIM(PLOT)) = '33012' 
+ AND LTRIM(RTRIM(HIDE)) IN   ('0002')
 
 --SELECT * FROM CCJOBHDR_SQL WHERE  LTRIM(RTRIM(jobno)) IN ('11111' ,'11002' )
 
 SELECT  * FROM cccuthst_sql 
 WHERE LTRIM(RTRIM(COLOUR)) = 'FMCKTX7'  
-AND	LTRIM(RTRIM(lotno)) = '032781' 
- AND LTRIM(RTRIM(hideno))   IN ('0056')
+AND	LTRIM(RTRIM(lotno)) = '033012' 
+ AND LTRIM(RTRIM(hideno))   IN ('0600','0601','0602','0603','0604','0605','0606')
 ORDER BY HIDENO ASC
 
 SELECT  * FROM RP_SC
@@ -38,25 +39,45 @@ AND LTRIM(RTRIM(HIDE)) IN   ('0631')
 
 
 /*
+USE DATA_02Pruebas
 UPDATE cccuthst_sql 
-	SET hidesqm = '22.00'
+	SET hidesqm = '15.10'
 WHERE LTRIM(RTRIM(COLOUR)) = 'FMCKTX7'  
-AND	LTRIM(RTRIM(lotno)) = '032781' 
- AND LTRIM(RTRIM(hideno))   IN ('0056')
+AND	LTRIM(RTRIM(lotno)) = '033012' 
+AND LTRIM(RTRIM(hideno))   IN ('0599')
 */
- 
- 
- SELECT * FROM RP_SC
- WHERE LTRIM(RTRIM(COLOUR)) = 'FMCKTX7'
- AND	LTRIM(RTRIM(HIDE)) NOT IN (	select HIDE from HIDESHDR_SQL   
-									inner join HIDESLIN_SQL on  HIDESLIN_SQL.FILENO = HIDESHDR_SQL.FILENO
-									)
 
-SELECT DISTINCT RP_SC.* FROM RP_SC
-INNER JOIN [PIEL_A_TRANSFERIR] ON LTRIM(RTRIM(RP_SC.COLOUR)) = [PIEL_A_TRANSFERIR].COLOR AND LTRIM(RTRIM(RP_SC.TAGNO))=[PIEL_A_TRANSFERIR].FOLIO_ORIGEN
-WHERE LTRIM(RTRIM(RP_SC.SQF)) <> [PIEL_A_TRANSFERIR].SQF_RECIBIR
-AND K_PIEL_A_TRANSFERIR_TRANSACCION = 1
 
-SELECT * FROM [PIEL_A_TRANSFERIR]
-WHERE SQF_ENVIAR <> [PIEL_A_TRANSFERIR].SQF_RECIBIR
-AND K_PIEL_A_TRANSFERIR_TRANSACCION = 1
+
+ --USE DATA_02Pruebas
+ --IF (	SELECT COUNT(*) FROM sys.table_types
+	--						WHERE name = 'TVPParamtest' AND schema_id = 1	) = 0
+	--						CREATE TYPE dbo.TVPParamtest AS TABLE(	COLOR		VARCHAR(20),
+	--															LOTE		VARCHAR(20),
+	--															SQF			VARCHAR(20)	)
+				
+select * from test
+ USE DATA_02Pruebas
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PG_PM_TEST_TABLA]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[PG_PM_TEST_TABLA]
+GO
+-- EXECUTE   [dbo].[PG_PM_TEST_TABLA] 0,0, 'B' , 'FCMKDX9' 
+CREATE PROCEDURE [dbo].[PG_PM_TEST_TABLA]
+	--@PP_K_SISTEMA_EXE			INT,
+	--@PP_K_USUARIO_ACCION		INT,
+	-- ===========================
+	@TBL_TOTAL_SQF_X_LOTE		TVPParamtest READONLY
+	-- ===========================
+AS
+	-- ///////////////////////////////////////////
+
+	select * into test
+	from @TBL_TOTAL_SQF_X_LOTE
+	-- ////////////////////////////////////////////////////////////////////
+
+
+	-- ////////////////////////////////////////////////////////////////////
+	-- ////////////////////////////////////////////////////////////////////
+
+GO
+
