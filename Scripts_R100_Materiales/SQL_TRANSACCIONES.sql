40038d6746dde1f7770c79528845b8635098a908
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-07-20 17:37:20 -0600

    CAMBIOS PARA FOLIOS 20072020

--- Scripts_R100_Materiales/SQL_TRANSACCIONES.sql
+++ Scripts_R100_Materiales/SQL_TRANSACCIONES.sql
@@ -5,16 +5,17 @@ PIEL_LOG.*
 FROM PIEL_LOG 
 INNER JOIN users_pearl ON PIEL_LOG.K_USUARIO_ALTA = users_pearl.codigo
 INNER JOIN TIPO_PIEL_LOG ON PIEL_LOG.K_TIPO_PIEL_LOG=TIPO_PIEL_LOG.K_TIPO_PIEL_LOG
-WHERE ORDEN_ORIGEN = 13600 or ORDEN_DESTIDO = 13620
-and FOLIO_ORIGEN = 3791088
---PIEL = 345
---AND LOTE = 32931
+WHERE 
+ORDEN_ORIGEN = 13600 or ORDEN_DESTIDO = 13600
+--and FOLIO_ORIGEN = 3791088
+--PIEL = 188
+--AND LOTE = 33182
 --AND COLOR = 'FMCKTX7' 
---ORDER BY F_ALTA asc
+--ORDER BY K_PIEL_LOG asc
 
-SELECT * FROM  RP_SC WHERE TAGNO IN (3791158)
+SELECT * FROM  RP_SC WHERE TAGNO IN (3791088)
 
-SELECT * FROM	RP_FOLIOS WHERE TAG IN (3791158)
+SELECT * FROM	RP_FOLIOS WHERE TAG IN (3791088)
 
 --WHERE HIDE  IN (0764)
 --AND LOT = '105081'
@@ -53,38 +54,33 @@ SELECT * FROM ccjobhdr_sql WHERE folio = '3791158'
 SELECT * FROM ccjobhdr_sql WHERE jobno = '12867'
 SELECT * FROM ccjobhdr_sql WHERE jobno = '12851'
 
-SELECT top 10 * FROM IMLSTRX_SQL	WHERE LTRIM(RTRIM(ord_no)) IN ( '04718701',
-'04716177',
-'04713298',
-'04713839',
-'04711148',
-'04711149',
-'04711307')
+USE DATA_02
+SELECT TOP 1000 D_TIPO_PIEL_LOG,usuario,
+PIEL_LOG.*  
+FROM PIEL_LOG 
+INNER JOIN users_pearl ON PIEL_LOG.K_USUARIO_ALTA = users_pearl.codigo
+INNER JOIN TIPO_PIEL_LOG ON PIEL_LOG.K_TIPO_PIEL_LOG=TIPO_PIEL_LOG.K_TIPO_PIEL_LOG
+WHERE 
+ORDEN_ORIGEN = 13600 --or ORDEN_DESTIDO = 13600
 
-SELECT top 10 * FROM IMINVTRX_SQL	WHERE LTRIM(RTRIM(ord_no)) IN ( '4718701',
-'4716177',
-'4713298',
-'4713839',
-'4711148',
-'4711149',
-'4711307')
+SELECT top 10 * FROM IMLSTRX_SQL	WHERE LTRIM(RTRIM(ord_no)) IN ( '4721463  ')
+
+SELECT top 10 * FROM IMINVTRX_SQL	WHERE LTRIM(RTRIM(ord_no)) IN ( '04720615 ','4720503 ')
 
 select top 1000 * --ord_no,lev_no,doc_type, item_no, loc,quantity,doc_ord_no,trx_dt,trx_tm, user_name, A4GLIdentity
 from IMINVTRX_SQL 
-where  item_no = 'FCPRDX9        '    and loc = 'T10' and trx_dt>='20200129'
-and doc_ord_no = '13222'
+where  item_no = 'FMCKTX7        '    /*and loc = 'T45'*/ and trx_dt>='20200714'
+and doc_ord_no = '13620'
 --and ord_no in ( '4642651','4642598')
-order by trx_dt desc
+order by trx_dt, ORD_NO desc
 --order by ord_no asc
 
 /*
-original entraron 1530.3 y se le restaron 65.4 orden 13222
 
-UPDATE IMLSTRX_SQL
-	SET trx_qty= trx_qty - 65.40
-WHERE ord_no = '04711148' 
+UPDATE IMINVTRX_SQL
+	SET LOC= 'T45'
+WHERE ord_no = '4721463' 
 AND LEV_NO = 1
-and ser_lot_no = '         105131'
 
 */
 
